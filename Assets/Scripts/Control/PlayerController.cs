da1421392b66889fa08614fadc1974325cca7f07
Barbara Lócsi <locsi.barbara@gmail.com>
2022-09-26 18:53:16 +0200

    #25: Use block scoped namespaces (old C# version)

--- Assets/Scripts/Control/PlayerController.cs
+++ Assets/Scripts/Control/PlayerController.cs
@@ -1,25 +1,27 @@
 using RPG.Movement;
 using UnityEngine;
-namespace RPG.Control;
 
-public class PlayerController : MonoBehaviour {
+namespace RPG.Control
+{
+    public class PlayerController : MonoBehaviour {
 
-    private void Update() {
-        if(Input.GetMouseButton(0))
-        {
-            MoveToCursor();
+        private void Update() {
+            if(Input.GetMouseButton(0))
+            {
+                MoveToCursor();
+            }
         }
-    }
 
-    private void MoveToCursor()
-    {
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-
-        bool hasHit = Physics.Raycast(ray, out var hitInfo);
-        if(hasHit)
+        private void MoveToCursor()
         {
-            GetComponent<Mover>().MoveTo(hitInfo.point);
-        }
-    }
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 
+            bool hasHit = Physics.Raycast(ray, out var hitInfo);
+            if(hasHit)
+            {
+                GetComponent<Mover>().MoveTo(hitInfo.point);
+            }
+        }
+
+    }
 }
--- Assets/Scripts/Movement/Mover.cs
+++ Assets/Scripts/Movement/Mover.cs
@@ -3,34 +3,36 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
-namespace RPG.Movement;
 
-public class Mover : MonoBehaviour
+namespace RPG.Movement
 {
-    [SerializeField]
-    Transform target;
-
-    // Update is called once per frame
-    void Update()
+    public class Mover : MonoBehaviour
     {
-        UpdateAnimator();
-    }
+        [SerializeField]
+        Transform target;
 
-    public void MoveTo(Vector3 destination)
-    {
-        GetComponent<NavMeshAgent>().destination = destination;
-    }
+        // Update is called once per frame
+        void Update()
+        {
+            UpdateAnimator();
+        }
 
-    private void UpdateAnimator()
-    {
-        Vector3 velocity = GetComponent<NavMeshAgent>().velocity;
+        public void MoveTo(Vector3 destination)
+        {
+            GetComponent<NavMeshAgent>().destination = destination;
+        }
 
-        // Convert this to the local value relative to the character - taking from global converting to local
-        Vector3 localVelocity = transform.InverseTransformDirection(velocity);
+        private void UpdateAnimator()
+        {
+            Vector3 velocity = GetComponent<NavMeshAgent>().velocity;
 
-        float speed = localVelocity.z;
+            // Convert this to the local value relative to the character - taking from global converting to local
+            Vector3 localVelocity = transform.InverseTransformDirection(velocity);
 
-        // Set the animators blend valut to be equal to our desired forward speed (on the Z axis)
-        GetComponent<Animator>().SetFloat("forwardSpeed", speed);
+            float speed = localVelocity.z;
+
+            // Set the animators blend valut to be equal to our desired forward speed (on the Z axis)
+            GetComponent<Animator>().SetFloat("forwardSpeed", speed);
+        }
     }
 }
