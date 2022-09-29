832a7d1dadd2b5ed88d86efc7cc91fb721b5366e
Barbara Lócsi <locsi.barbara@gmail.com>
2022-09-29 19:29:10 +0200

    #45: Calculating AI chase distance

---
+++ Assets/Scripts/Control/AIController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.Control
+{
+    public class AIController : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        [SerializeField] float chaseDistance = 5f;
+
+        private void Update() {
+            var player = GameObject.FindWithTag("Player");
+            var distanceToPlayer = Vector3.Distance(transform.position, player.transform.position);
+
+            if(distanceToPlayer < chaseDistance)
+            {
+                print($"{this}: chase the player!");
+            }
+        }
+    }
+}
